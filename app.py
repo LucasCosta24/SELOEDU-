17f13954bc22de0f88f9c4fdffa4d8802776655e
LucasCosta24 <lucas240303@gmail.com>
2025-10-11 12:35:31 -0300

    Update projeto users e profile

--- app.py
+++ app.py
@@ -1,18 +1,48 @@
-from flask import Flask, render_template, redirect, url_for
+# app.py
+from flask import Flask, render_template
+from models.users import db, User
+from extensions import login_manager
+from routes.auth import auth_bp
+from routes.users import users_bp
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'sua_chave_secreta'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///seloedu.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@app.route("/")
-def home():
-    return render_template("home.html")
+    db.init_app(app)
+    login_manager.init_app(app)
 
-@app.route("/login")
-def login():
-    return render_template("auth/login.html")
+    # registrar blueprints
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(users_bp)
 
-@app.route("/do_login", methods=["POST"])
-def do_login():
-    return redirect(url_for("home"))
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    # cria usuário master se não existir
+    with app.app_context():
+        db.create_all()
+        if not User.query.filter_by(email="admin@seloedu.com").first():
+            master = User(
+                nome="Admin Master",
+                email="admin@seloedu.com",
+                role="master"
+            )
+            master.set_password("123456")
+            db.session.add(master)
+            db.session.commit()
+
+    # rota principal
+    @app.route("/")
+    def home():
+        return render_template("home.html")
+
+    return app
+
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True)
